548d3178b25776f1a0688bac51edffe8f513ed19
Amena <syedaamenakauser@gmail.com>
2018-07-08 17:50:30 -0400

    init

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -6,6 +7,8 @@ const PORT = 3000;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
 app.listen(process.env.PORT !== undefined ? process.env.PORT : PORT, () => {
   console.log(`listening on port ${PORT}`);
