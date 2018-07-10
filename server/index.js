106a9466e4239f147a3ac54a884eb50bb068b010
Ted Green <ted.green29@gmail.com>
2018-07-10 16:53:07 -0700

    pull upstream master

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const db = require('../db/helpers.js');
 
@@ -8,8 +9,7 @@ const PORT = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
-const homePath = __dirname + '/../client/dist';
-app.use(express.static(homePath));
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
 app.listen(process.env.PORT !== undefined ? process.env.PORT : PORT, () => {
   console.log(`listening on port ${PORT}`);
