7d765771dd5f0a6ad060539563b545644868c289
Amena <syedaamenakauser@gmail.com>
2018-07-10 23:52:14 -0400

    Add table to store express sessions

--- db/Models/Session.js
+++ db/Models/Session.js
@@ -1,15 +1,15 @@
-var session = require('express-session');
-var connection = require('../../db');
+const session = require('express-session');
+const connection = require('../../db');
+
+connection.sync();
 
 // initalize sequelize with session store
-var SequelizeStore = require('connect-session-sequelize')(session.Store);
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-var sessionStore = new SequelizeStore({
+const sessionStore = new SequelizeStore({
   db: connection,
   checkExpirationInterval: 15 * 60 * 1000, // The interval at which to cleanup expired sessions in milliseconds.
   expiration: 1 * 60 * 60 * 1000  // The maximum age (in milliseconds) of a valid session.
 })
-
-console.log('making session store', sessionStore)
 
 module.exports = sessionStore;
