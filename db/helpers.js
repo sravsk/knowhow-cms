f83f24c0ea41a3ff105de847eb382ddb1932e1e1
Amena <syedaamenakauser@gmail.com>
2018-07-10 22:38:31 -0400

    Fix databse helper function to catch duplicate email error

--- db/helpers.js
+++ db/helpers.js
@@ -23,35 +23,46 @@ var dbHelpers = {
   // check if a user exists for the given company, if yes, return false in callback
   // if no user exists for the given company, create a user with role 'admin', return true in callback
   addUser: ({name, email, password, company, domain}, cb) => {
-    Company.findOne({
-      where: {domain: domain}
-    }).
-    then(result => {
-      if (result === null) {
-        // create company
-        Company.create({
-          name: company,
-          domain: domain
-        })
-        .then(result => {
-          let companyId = result.id;
-          // create user
-          User.create({
-            name: name,
-            email: email,
-            password: password,
-            role: 'admin',
-            companyId: companyId
-          })
-          .then(user => {
-            cb(true);
-          })
-        })
+    User.findOne({
+      where: {email: email}
+    })
+    .then(result => {
+      if (result !== null) {
+        // a user already exists with this email
+        cb(email, true)
       } else {
-        // an admin user exists for the given company
-        cb(false);
+        // email is unique
+        Company.findOne({
+          where: {domain: domain}
+        }).
+        then(result => {
+          if (result === null) {
+            // create company
+            Company.create({
+              name: company,
+              domain: domain
+            })
+            .then(result => {
+              let companyId = result.id;
+              // create user
+              User.create({
+                name: name,
+                email: email,
+                password: password,
+                role: 'admin',
+                companyId: companyId
+              })
+              .then(user => {
+                cb(true, null);
+              })
+            })
+          } else {
+            // an admin user exists for the given company
+            cb(false, null);
+          }
+        })
       }
-    });
+    })
   },
 
   // updating a user's password, during forgot pw or after invitation
