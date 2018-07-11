79a9e7595741cebef59f479967e3ff005bae59fa
Amena <syedaamenakauser@gmail.com>
2018-07-10 22:09:45 -0400

    Add /signupuser route to add a new user and hashed password in database

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 const db = require('../db/helpers.js');
 
 const app = express();
 const PORT = 3000;
+const saltRounds = 10;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
@@ -13,6 +15,30 @@ app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/../client/dist/index.html'));
+});
+
+app.post('/signupuser', (req, res) => {
+  let password = req.body.password;
+  // hash the password by auto-gen a salt and hash
+  bcrypt.hash(password, saltRounds, function(err, hash) {
+    // store hash in database
+    if (hash) {
+      db.addUser({
+        name: req.body.name,
+        email: req.body.email,
+        password: hash,
+        company: req.body.company,
+        domain: req.body.domain
+      }, function(userCreated, error) {
+        if (userCreated) {
+          res.send('user created');
+        } else {
+          res.send('user already exists for this company');
+        }
+      });
+    }
+  });
+
 });
 
 app.listen(process.env.PORT !== undefined ? process.env.PORT : PORT, () => {
