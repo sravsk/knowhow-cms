7a4e21c86ac3762f4e2bca1a3f7a9a514f674c53
Amena <syedaamenakauser@gmail.com>
2018-07-11 16:37:59 -0400

    Use NavBar

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
+import NavBar from './NavBar.jsx';
 
 const Dashboard = () => {
   return (
     <Container>
+    <NavBar />
     <div>This is the Dashboard page</div>
     </Container>
   );
