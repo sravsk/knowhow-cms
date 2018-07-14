c9162a2729a07c611099d3a500ffac70c86b1b37
Amena <syedaamenakauser@gmail.com>
2018-07-14 15:19:17 -0400

    Change datatype of 'content' in articles table to text

--- db/Models/Article.js
+++ db/Models/Article.js
@@ -5,7 +5,7 @@ const Article = db.define('articles', {
   id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
   title: { type: Sequelize.STRING, allowNull: false },
   description: { type: Sequelize.STRING, allowNull: false },
-  content: {type: Sequelize.STRING, allowNull: false}
+  content: {type: Sequelize.TEXT, allowNull: false}
 });
 
 module.exports = Article;
