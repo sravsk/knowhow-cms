41fb3155067af7588fa418b15ee628ca4b4a3d83
Ted Green <ted.green29@gmail.com>
2018-07-14 18:36:17 -0700

    (update) Article Component

--- client/src/components/NewArticle.jsx
+++ client/src/components/NewArticle.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Container, Input, Form } from 'semantic-ui-react';
+import { Container, Form, Button, Input, Dropdown } from 'semantic-ui-react';
 import Editor from './Editor.jsx';
+import ReactQuill from 'react-quill';
+import axios from 'axios';
 
 class NewArticle extends React.Component{
   constructor(props) {
@@ -8,43 +10,149 @@ class NewArticle extends React.Component{
     this.state = {
       title: '',
       description: '',
-      content: ''
+      content: '',
+      category: '',
+      categories: [],
+      modules: {
+        toolbar: [
+          [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+          [{size: []}],
+          ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+          [{'list': 'ordered'}, {'list': 'bullet'},
+           {'indent': '-1'}, {'indent': '+1'}],
+          ['link', 'image', 'video'],
+          ['clean']
+        ],
+        clipboard: {
+          matchVisual: false,
+        }
+      },
+      formats: [
+        'header', 'font', 'size',
+        'bold', 'italic', 'underline', 'strike', 'blockquote',
+        'list', 'bullet', 'indent',
+        'link', 'image', 'video'
+      ]
     }
+
+    this.setState = this.setState.bind(this);
     this.handleContentChange = this.handleContentChange.bind(this);
+    this.handleTitleChange = this.handleTitleChange.bind(this);
+    this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
+    this.handleSelectCategory = this.handleSelectCategory.bind(this);
+    this.saveArticle = this.saveArticle.bind(this);
   }
 
-  handleTitleChange(e){
-    this.setState({
-      title: e
+  componentDidMount() {
+    axios.get('/user').then(res => {
+      let compId = res.data.companyId;
+      axios.get(`/${compId}/categoriesdata`).then(res2 => {
+        var cleaned = []
+        res2.data.forEach(catOption => {
+          let scrubbed = {
+            text: catOption.name,
+            value: catOption.id,
+            description: catOption.description
+          }
+          cleaned.push(scrubbed);
+        })
+        this.setState({categories: cleaned})
+      })
     })
   }
 
-  handleDescriptionChange(e){
+  handleTitleChange(e, { value }) {
     this.setState({
-      description: e
+      title: value
+    })
+  }
+
+  handleDescriptionChange(e, { value }) {
+    this.setState({
+      description: value
     })
   }
 
   handleContentChange(html) {
-    this.setState({content: html})
+    this.setState({
+      content: html
+    })
+  }
+
+  handleSelectCategory(e, { value }) {
+    this.setState({
+      category: value
+    }, () => console.log(this.state.category))
+  }
+
+  saveArticle() {
+    let obj = {
+      title: this.state.title,
+      description: this.state.description,
+      content: this.state.content,
+      categoryId: this.state.category
+    };
+
+    console.log(obj)
+    axios.post('/article',obj).then(res => {
+      alert(res.data);
+    });
   }
 
   render() {
     return (
       <Container>
+        <h1>New Article</h1>
+        <Dropdown
+          button
+          search
+          labeled
+          fluid
+          floating
+          className='icon'
+          icon='bars'
+          text='Select Category'
+          options={this.state.categories}
+          value={this.state.category}
+          onChange={this.handleSelectCategory}
+        />
+        <br />
         <Form>
-          <Form.Field>
-            <label>Title</label>
-            <input placeholder='New Article Title' onChange={this.handleTitleChange}/>
-          </Form.Field>
-          <Form.Field>
-            <label>Description</label>
-            <input placeholder='Article description' onChange={this.handleDescriptionChange}/>
-          </Form.Field>
-          <Form.Field>
-            <label>Content</label>
-            <Editor handleContentChange={this.handleContentChange} content={this.props.content}/>
-          </Form.Field>
+          <h3>Title</h3>
+          <Input
+            placeholder='New Article Title'
+            onChange={this.handleTitleChange}
+          />
+          <br />
+          <br />
+
+          <h3>Description</h3>
+          <Input
+            placeholder='Article description'
+            onChange={this.handleDescriptionChange}
+            fluid
+          />
+          <br />
+
+          <h3>Content</h3>
+          <ReactQuill
+            className="editor"
+            placeholder={"Contribute content . . . "}
+            className="content"
+            ref={(el) => { this.reactQuillRef = el }}
+            modules={this.state.modules}
+            formats={this.state.formats}
+            theme="snow"
+            bounds={'.editor'}
+            onChange={this.handleContentChange}
+            value={this.state.content}
+          />
+          <br />
+
+          <Button
+            content='Save Article'
+            onClick={this.saveArticle}
+          />
         </Form>
       </Container>
     )
