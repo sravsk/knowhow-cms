5c12fbab7dc7bff1bd135cff033bcca0cfd976ca
Amena <syedaamenakauser@gmail.com>
2018-07-18 18:47:27 -0400

    Restyle ArticleContent component

--- client/src/components/ArticleContent.jsx
+++ client/src/components/ArticleContent.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import { Container, Item, Button } from 'semantic-ui-react';
+import { Container, Item, Button, Segment, Header } from 'semantic-ui-react';
 
 const ArticleContent = (props) => {
   let articleId = props.match.params;
   let article = props.location.state.article;
   return (
     <Container>
-      <Item>
-        <Button floated='right'>Edit Article</Button>
-        <Item.Header as='h3'>
+      <br /><br />
+      <Button floated='right'>Edit Article</Button>
+      <Segment style={{ 'marginTop': '7vh' }}>
+        <Header as='h3'>
           {article.title}
-        </Item.Header>
+        </Header>
         <Item.Description as='h5'>
           {article.description}
         </Item.Description>
         <Item.Extra>
           {article.content}
         </Item.Extra>
-      </Item>
+      </Segment>
     </Container>
   );
 }
