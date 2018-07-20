32b5ce19b5a6a61d7b54cf7742d78a1fe8b8cfb9
Amena <syedaamenakauser@gmail.com>
2018-07-19 20:37:12 -0400

    Add styles for articles/add new category

--- client/src/components/ArticleContent.jsx
+++ client/src/components/ArticleContent.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Container, Item, Button, Segment, Header } from 'semantic-ui-react';
+import NavBar from './NavBar.jsx';
 
 const ArticleContent = (props) => {
   let articleId = props.match.params;
   let article = props.location.state.article;
   return (
-    <Segment style={{ 'minHeight': '70vh' }}>
-      <br />
+    <Segment raised  style={{ 'marginTop': '-5em', 'minHeight': '100vh'}}>
+      <NavBar />
       <Button floated='right'>Edit Article</Button>
-      <Header as='h2' attached='top' style={{'marginTop': '7vh'}} >
-        {article.title}
-      </Header>
-      <Header as='h3' attached='top, bottom'>
+      <Header as='h3' textAlign='center'>
+        {article.title}<br/>
         {article.description}
       </Header>
-      <Segment style={{ 'minHeight': '60vh' }}>
+      <Segment style={{ 'minHeight': '80vh' }}>
         {article.content}
       </Segment>
     </Segment>
