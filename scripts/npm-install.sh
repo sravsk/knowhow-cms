975372c62efe475dca543de4d62c65be8a76dd72
sravsk <sravanthi.karanam@gmail.com>
2018-07-24 19:10:47 -0700

    updated start script - fix merge conflict

--- scripts/npm-install.sh
+++ scripts/npm-install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /var/www/html/knowhow
 sudo npm install
+sudo ./node_modules/.bin/webpack --progress --colors --config webpack.config.js --bail
 
 # install pm2 module globaly
 sudo npm install -g pm2
