409db97406ebb7106246dde05673c9cae3fa37a3
housker <housker@gmail.com>
2018-08-16 22:22:12 -0400

    debugging content exceeds size limit

--- db/Models/Article.js
+++ db/Models/Article.js
@@ -4,8 +4,8 @@ const db = require('../index.js');
 const Article = db.define('articles', {
   id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
   title: { type: Sequelize.STRING, allowNull: false },
-  description: { type: Sequelize.STRING, allowNull: false },
-  content: {type: Sequelize.TEXT, allowNull: false}
+  description: { type: Sequelize.TEXT('long'), allowNull: false },
+  content: {type: Sequelize.TEXT('long'), allowNull: false}
 });
 
 module.exports = Article;
