b2a0b60a87586d4d5f776253dbecb5540463a38d
Amena <syedaamenakauser@gmail.com>
2018-08-18 14:51:49 -0400

    Modify function to index data to elasticsearch in batches of 10k

--- data/mysql-to-es-transfer.js
+++ data/mysql-to-es-transfer.js
@@ -1,18 +1,20 @@
 const client = require('../services/elasticsearch').client;
-const fetchArticles = require('../db/helpers').fetchArticles;
+const fetchAllArticles = require('../db/helpers').fetchAllArticles;
 
 // elasticsearch variables - index and type
 const index = 'articles';
 const type = 'documents';
 
-// enter company and category ids for bulk transfer of data
-const companyId = require('./fakeData').companyId;
-const categoryId = require('./fakeData').categoryId;
+// console.log('DATE1', new Date().toLocaleString())
 
-// function to bulk insert data into elasticsearch cluster from articles table
-const insertDataMySQLtoES = () => {
-  fetchArticles({companyId: companyId, categoryId: categoryId}, results => {
-    var articles = results.map(item => item.dataValues);
+const insertDataMySQLtoES = async () => {
+  let results = await fetchAllArticles();
+  // index data in batches of 10k
+  let i = 0;
+  let len = results.length;
+  while (i <= len) {
+    let articles = results.slice(i, i + 10000);
+    console.log('articles length', articles.length, i)
     let bulkBody = [];
     articles.forEach(item => {
       bulkBody.push({
@@ -20,12 +22,10 @@ const insertDataMySQLtoES = () => {
       });
       bulkBody.push({ id: item.id, title: item.title, description: item.description, content: item.content, companyid: item.companyId, categoryid: item.categoryId });
     });
-    client.bulk({body: bulkBody})
-      .then(response => {
-        console.log('response', response)
-      })
-  });
+    await client.bulk({body: bulkBody});
+    i += 10000;
+  }
 };
 
-insertDataMySQLtoES()
+// insertDataMySQLtoES()
 
