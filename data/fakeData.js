9f3e6e320952442b3cb248d61a9d7e3cf5250254
Amena <syedaamenakauser@gmail.com>
2018-08-23 14:24:19 -0400

    Modify fakeData.js to write fake articles data to a file

--- data/fakeData.js
+++ data/fakeData.js
@@ -1,35 +1,44 @@
 const randomWords = require('random-words');
+const fs = require('fs');
 
-const company = 'Send Grid';
-const domain = 'sendgrid.com';
+let articlesStream = fs.createWriteStream('./articles.txt');
 
-let companyId = 4;
-let categoryId = 14;
+// const company = 'Send Grid';
+// const domain = 'sendgrid.com';
+
+let companyId = 2;
+let categoryId = 7;
+
+let length = 1000000;
 
 const generateData = (length) => {
   let data = [];
   for (let i = 0; i < length; i++) {
-    // if ((i % 1000) === 0) {
-    //   console.log('COUNT', i+1)
-    // }
-    let article = {
-      title: randomWords({ min: 1, max: 6 }).join(' '),
-      description: randomWords({ min: 3, max: 10 }).join(' '),
-      content: '',
-      companyId: companyId,
-      categoryId: categoryId
-    }
+    let article = [];
+    let title = randomWords({ min: 1, max: 6 }).join(' ');
+    let description = randomWords({ min: 3, max: 10 }).join(' ');
+    let content = '';
     let contentLength = Math.floor(Math.random() * 20) + 2;
-    content = '';
     for (let i = 0; i < contentLength; i++) {
       content += randomWords({ min: 3, max: 8 }).join(' ') + '.' + ' ';
     }
-    article.content = content;
+    article = [title, description, content, companyId, categoryId];
     data.push(article);
   }
   return data;
 };
 
-module.exports = { generateData, companyId, categoryId };
+let articlesCsv = generateData(length).join('\n');
+
+articlesStream.write(articlesCsv, 'utf8');
+articlesStream.on('finish', () => {
+  console.log(`DONE WRITING ${length} articles to file.`)
+})
+
+articlesStream.end();
+
+
+
+
 
 
