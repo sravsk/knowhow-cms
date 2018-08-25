bbf3b45829556b624aee2b52978bf6669b8b6c48
Amena <syedaamenakauser@gmail.com>
2018-08-25 10:55:03 -0400

    Optimize reading data from mysql table to transfer to ES index

--- data/mysql-to-es-transfer.js
+++ data/mysql-to-es-transfer.js
@@ -1,35 +1,44 @@
 const client = require('../services/elasticsearch').client;
-const fetchAllArticles = require('../db/helpers').fetchAllArticles;
+const connection = require('../db');
 
 // elasticsearch variables - index and type
 const index = 'articles';
 const type = 'documents';
 
 const insertDataMySQLtoES = async () => {
-  let results = await fetchAllArticles();
-  console.log('results', results.length)
-  // index data in batches of 5k
-  let i = 0;
-  let len = results.length;
-  while (i <= len) {
-    let articles = results.slice(i, i + 5000);
-    // console.log('articles length', articles.length, i)
-    let bulkBody = [];
-    articles.forEach(item => {
-      bulkBody.push({
-        index: { _index: index, _type: type, _id: item.id }
-      });
-      bulkBody.push({ id: item.id, title: item.title, description: item.description, content: item.content, companyid: item.companyId, categoryid: item.categoryId });
-    });
-    try {
-      await client.bulk({body: bulkBody});
-      // console.log('5000 records done', i)
-    } catch(err) {
-      console.log(err);
+  let maxReturnedId = 0;
+  connection.query('SELECT MAX(id) FROM articles')
+  .then(async(result) => {
+    let maxId = result[0][0]['MAX(id)'];
+    while (maxReturnedId < maxId) {
+      // transfer data from mysql articles table to elasticsearch index in batches
+      let queryString = `SELECT id, title, content, description, categoryId, companyId FROM articles WHERE id > ${maxReturnedId} ORDER BY id ASC LIMIT 5000;`
+      try {
+        await connection.query(queryString)
+        .then(async(response) => {
+          let articles = response[0];
+          let bulkBody = [];
+          articles.forEach(item => {
+            bulkBody.push({
+              index: { _index: index, _type: type, _id: item.id }
+            });
+            bulkBody.push({ id: item.id, title: item.title, description: item.description, content: item.content, companyid: item.companyId, categoryid: item.categoryId });
+          });
+          try {
+            await client.bulk({body: bulkBody});
+            maxReturnedId += 5000;
+            // console.log('5000 records done', maxReturnedId)
+          } catch(err) {
+            console.log('error saving records to elasticsearch', err);
+          }
+        })
+      } catch(err) {
+        console.log('error querying articles table', err);
+      }
     }
-    i += 5000;
-  }
+  }).catch(err => {
+    console.log('error reading max id', err);
+  })
 };
 
 insertDataMySQLtoES()
-
